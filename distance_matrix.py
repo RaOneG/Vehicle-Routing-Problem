3375e7e2bae6c8ee75d0a67dc500ee463baa2757
RaOneG <rawan.gafar21@gmail.com>
2021-09-20 12:26:55 +0200

    initial commit

--- distance_matrix.py
+++ distance_matrix.py
@@ -64,7 +64,7 @@ def send_request(origin_addresses, dest_addresses, API_key):
   request = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial'
   origin_address_str = build_address_str(origin_addresses)
   dest_address_str = build_address_str(dest_addresses)
-  request = request + '&origins=' + origin_address_str + '&destinations=' + dest_address_str + '&key=' + API_key
+  request = str(request) + '&origins=' + str(origin_address_str) + '&destinations=' + str(dest_address_str) + '&key=' + API_key
   jsonResult = urllib.request.urlopen(request).read()
   response = json.loads(jsonResult)
   return response
@@ -77,3 +77,4 @@ def build_distance_matrix(response):
     distance_matrix.append(row_list)
   return distance_matrix
 
+print(create_distance_matrix())
